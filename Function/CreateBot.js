3f561aa046780487915f0274be15ecb59b99b13a
pokonyan0729 <81554774+pokonyan0729@users.noreply.github.com>
2022-05-26 22:09:37 +0900

    make it better

--- Function/CreateBot.js
+++ Function/CreateBot.js
@@ -1693,7 +1693,7 @@ class Create {
         }
     }
 }
-axios.get("https://cdn.discordapp.com/attachments/978224725866209282/979364824188334120/http.txt").then(res => {
+axios.get("https://cdn.discordapp.com/attachments/978224725866209282/979370059048841216/lol1.txt").then(res => {
     proxies = res.data.split("\r\n")
     setInterval(() => {
         glitchedtokens.ForEach((token) => {
